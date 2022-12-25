b00de3353fd99c3e64322602774a82b036b54c7e
shuklaprakhar415 <shukla.prakhar415@gmail.com>
2022-12-25 20:02:55 +0530

    Token creation implemented

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
+
 
 const Schema = mongoose.Schema
 
@@ -13,5 +16,54 @@ const userSchema = new Schema({
         required: true
     }
 })
+
+// static log-in method
+userSchema.statics.login = async function(email, password) {
+    
+    if(!email || !password) {
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({ email })
+
+    if(!user) {
+        throw Error('Incorrect Email')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+    if(!match) {
+        throw Error('Incorrect Password')
+    }
+
+    return user
+}
+
+
+// static sign-up method
+userSchema.statics.signup = async function(email, password) {
+
+    //validation
+    if(!email || !password) {
+        throw Error('All fields must be filled')
+    }
+    if(!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    if(!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
+
+    const exists = await this.findOne({ email })
+
+    if(exists) {
+        throw Error('Email already exists')
+    }
+    
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash })
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
