3d64dc380bdd004c58769cc2f5b2c8a7235e5670
shuklaprakhar415 <shukla.prakhar415@gmail.com>
2022-12-29 19:09:49 +0530

    Auth added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,35 @@
 import { Link } from 'react-router-dom'
+import {useLogout} from '../hooks/useLogout'
+import {useAuthContext} from '../hooks/useAuthContext'
 
 const Navbar = () => {
+
+    const { logout } = useLogout()
+    const { user } = useAuthContext()
+
+
+    const handleClick = () => {
+        logout()
+    }
+
     return (
         <header>
             <div className="container">
                 <Link to='/'>
-                    <h1>Workout Buddy</h1>
+                    <h1>Workouts</h1>
                 </Link>
                 <nav>
-                    <div>
+                    {user && (<div>
+                        <span>{user.email}</span>
+                        <button onClick={handleClick}>Log out</button>
+                    </div>
+                    )}
+
+                    {!user && (<div>
                         <Link to="/login">Login</Link>
                         <Link to="/signup">Signup</Link>
                     </div>
+                    )}
                 </nav>
             </div>
         </header>
